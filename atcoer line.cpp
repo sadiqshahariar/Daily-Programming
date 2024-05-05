865a258ab9271bc5b424491ac57f1d06e849f419
sadiqshahariar <sadiqshahariar@gmail.com>
2024-05-05 09:52:42 +0600

    Atcoder solution

---
+++ atcoer line.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using  namespace  std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define sq(x)   (x)*(x)
+#define PI      acos(-1.0)
+#define all(x) x.begin(),x.end()
+#define nl      '\n'
+#define pi          acos(-1.0)
+#define mod               1000000007
+
+typedef long long int ll;
+typedef unsigned long long int  llu;
+
+int main()
+{
+    int n,a,b,c;
+    cin>>n>>a>>b>>c;
+    if(c>=min(a,b) && c<=max(a,b))cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
---
+++ typing.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define sq(x)   (x)*(x)
+#define PI      acos(-1.0)
+#define all(x) x.begin(),x.end()
+#define nl      '\n'
+#define pi          acos(-1.0)
+#define mod               1000000007
+
+typedef long long int ll;
+typedef unsigned long long int  llu;
+
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    vector<int>v;
+    int j=0;
+    for(int i=0;i<s.size();i++)
+    {
+           while(j<t.size() && s[i]!=t[j])j++;
+           v.push_back(j+1);
+    }
+    for(int i=0;i<v.size();i++)cout<<v[i]<<" ";
+    cout<<endl;
+}
